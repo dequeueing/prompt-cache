0ca0570dd136887d8095c6052e0ab8f586a52a0a
Taojie Wang <taojie.sustcs@gmail.com>
2025-01-17 21:40:06 +0800

    minor: delete some redudant code

--- demo_change.py
+++ demo_change.py
@@ -30,77 +30,14 @@ def main(enable_cache=True):
 
     lm = lm_for_cache
 
-    if enable_cpu_inference:
-        lm = CodeLlama("codellama/CodeLlama-7b-Instruct-hf",
-                       load_in_8bit=False,
-                       device_map=None)
-
-    # lm = Falcon("tiiuae/falcon-7b-instruct",
-    #             load_in_8bit=True if not disable_cuda else False,
-    #             device_map="auto" if not disable_cuda else None)
-
-    # lm = Mpt("mosaicml/mpt-7b-chat-8k",
-    #          load_in_8bit=True if not disable_cuda else False,
-    #          device_map="auto" if not disable_cuda else None)
-
     cache_engine = CacheEngine(5000, lm_for_cache, target_device='cpu' if enable_cpu_inference else None)
     gen_engine = GenerationEngine(lm)
 
     preproc = [
-        # CompactSpaces(),
         lm.get_formatter()
     ]
 
     cache_engine.add_schema(read_file("./examples/code_generation_game.xml", preproc), max_tokens=800)
-
-    parameter = GenerationParameters(
-        temperature=1.0,
-        repetition_penalty=1.0,
-        top_p=0.95,
-        top_k=-1,
-        max_new_tokens=512,
-        stop_token_ids=lm.stop_token_ids,
-        stop_str=lm.stop_str
-    )
-
-    prompt_text = f"""
-        <prompt schema='code-generation-game'>
-        <unit.py/>
-        <map.py/>
-        <player.py/>
-        <game.py/>
-        <database.py/>
-        <user>
-            Explain the code logic briefly.
-        </user>
-        </prompt>
-        """
-
-    prompt = Prompt(prompt_text, preproc)
-    token_ids, position_ids, cache_time, cache = cache_engine.process(prompt, no_cache=disable_prompt_cache,
-                                                                      return_full_position_ids=lm.use_full_position_ids)
-
-    output_stream = gen_engine.generate(token_ids, position_ids, parameter, cache, stream_interval=2,
-                                        use_full_position_ids=lm.use_full_position_ids)
-
-    print(f"Assistant: ", end="", flush=True)
-
-    resp = ""
-    pre = 0
-    for outputs in output_stream:
-        output_text = outputs.new_text.strip().split(" ")
-        now = len(output_text) - 1
-        if now > pre:
-            tt = " ".join(output_text[pre:now])
-            resp += tt + " "
-            print(tt, end=" ", flush=True)
-            pre = now
-    tt = " ".join(output_text[pre:])
-    print(tt, flush=True)
-    resp += tt
-
-    print("\n")
-    prompt_text += f"<assistant>{resp}</assistant>"
 
 
 def seed_everything(seed):
